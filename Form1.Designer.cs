7fa54770c2601c7278ee561af5f603f7c75a29ed
unknown <Administrator@.(none)>
2011-08-17 22:29:51 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace AsyncBlockingTcpListenerBug
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startListener = new System.Windows.Forms.Button();
+            this.sendMessage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startListener
+            // 
+            this.startListener.Location = new System.Drawing.Point(12, 17);
+            this.startListener.Name = "startListener";
+            this.startListener.Size = new System.Drawing.Size(95, 23);
+            this.startListener.TabIndex = 0;
+            this.startListener.Text = "Start Listener";
+            this.startListener.UseVisualStyleBackColor = true;
+            this.startListener.Click += new System.EventHandler(this.startListener_Click);
+            // 
+            // sendMessage
+            // 
+            this.sendMessage.Enabled = false;
+            this.sendMessage.Location = new System.Drawing.Point(113, 17);
+            this.sendMessage.Name = "sendMessage";
+            this.sendMessage.Size = new System.Drawing.Size(105, 23);
+            this.sendMessage.TabIndex = 1;
+            this.sendMessage.Text = "Send Message";
+            this.sendMessage.UseVisualStyleBackColor = true;
+            this.sendMessage.Click += new System.EventHandler(this.sendMessage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 57);
+            this.Controls.Add(this.sendMessage);
+            this.Controls.Add(this.startListener);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startListener;
+        private System.Windows.Forms.Button sendMessage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
+
+namespace AsyncBlockingTcpListenerBug
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener _listener;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startListener_Click(object sender, EventArgs e)
+        {
+            _listener = new TcpListener(IPAddress.Any ,54321);
+            _listener.Start();
+
+            sendMessage.Enabled = true;
+
+            Thread listenThread = new Thread(ListenForConnections);
+            listenThread.Start();
+//            ListenForConnections();
+        }
+
+        private void ListenForConnections()
+        {
+            while (true)
+            {
+                TcpClient client = _listener.AcceptTcpClient();
+
+                NetworkStream stream = client.GetStream();
+
+                while (client.Connected)
+                {
+
+                    byte[] data = new byte[1024];
+                    int length = 0;
+
+                    while (length < 1024)
+                    {
+                        data[length] = (byte)stream.ReadByte();
+
+                        if (data[length] == '\n')
+                            break;
+
+                        length++;
+                    }
+
+                    stream.Write(data, 0, length);
+                }
+
+                
+            }
+        }
+
+        private void sendMessage_Click(object sender, EventArgs e)
+        {
+            TcpClient client = new TcpClient("localhost", 54321);
+
+            NetworkStream stream = client.GetStream();
+
+            stream.Write(Encoding.UTF8.GetBytes("hello\n"), 0, 6);
+
+            byte[] buffer = new byte[6];
+            stream.Read(buffer, 0, 6);
+
+            string response = Encoding.UTF8.GetString(buffer);
+
+            MessageBox.Show(String.Format("Response was {0}", response));
+        }
+    }
+}
